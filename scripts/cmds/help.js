ab3f00306c0f9ed9db5d75bfaad1397be9b4e542
erenyeagerisgay3 <rifatahmed450@yahoo.com>
2025-03-21 12:37:38 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🦋 RiFaT 🌺";
+const doNotDelete = "✨ Rɪғᴀᴛ Aʜᴍᴇᴅ 🔥";
 /** 
 * @author NTKhang
 * @author: do not delete it
